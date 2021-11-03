7d7a4e0084bc96eecceceafa18ebc7484193e38b
Roman Ivanov <roman.ivanov.7@gmail.com>
2021-11-03 13:00:52 +0100

    disk fix

--- gcp_vm/create_disk.sh
+++ gcp_vm/create_disk.sh
@@ -2,7 +2,7 @@
 # Create disk from snapshot
 
 disk=$1
-snapshot=tfx-1
+snapshot=tfx-2
 
 gcloud beta compute disks create $disk --project=spark-optimization \
   --type=pd-ssd --size=80GB --zone=europe-west1-b \
